12e7f9a7ea5a8e93c16c2b5e54a948835f668ec6
pbehera-techrayslabs <pbehera@techrayslabs.com>
2026-03-17 12:59:08 +0530

    feat:added quantity to vehicle card

--- frontend/src/components/vehicleCard.ts
+++ frontend/src/components/vehicleCard.ts
@@ -1,24 +1,28 @@
 interface Vehicle {
 
-name: string
-price: number
-image: string
+    name: string
+    price: number
+    image: string
+    quantity: number
 
 }
 
 export function vehicleCard(vehicle: Vehicle) {
 
-return `
+    return `
 
-<div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transform hover:-translate-y-2 transition duration-300"><!-- Vehicle Image --><img src="${vehicle.image}" alt="${vehicle.name}"
+<div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-xl transform hover:-translate-y-2 transition duration-300"><img src="${vehicle.image}"
+alt="${vehicle.name}"
 class="w-full h-48 object-cover">
 
-<div class="p-5"><!-- Vehicle Name --><h3 class="text-xl font-bold text-gray-800">
+<div class="p-5"><h3 class="text-xl font-bold text-gray-800">
 ${vehicle.name}
-</h3><!-- Price --><p class="text-gray-500 mt-2 text-lg">
+</h3><p class="text-gray-500 mt-2 text-lg">
 ₹${vehicle.price} <span class="text-sm">/ day</span>
-</p><!-- Button --><button
-class="mt-4 w-full bg-linear-to-r from-blue-600 to-blue-500 text-white py-2 rounded-lg font-semibold hover:from-blue-700 hover:to-blue-600 transition duration-300">
+</p><p class="text-sm text-green-600">
+Available Units: ${vehicle.quantity}
+</p><button
+class="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg font-semibold hover:bg-blue-700 transition">
 
 🚗 Book Now
 
