6ac485cd092849bd2ed0597bf14053eae0c8417d
pbehera-techrayslabs <pbehera@techrayslabs.com>
2026-03-17 14:59:38 +0530

    fix: sweet alert function added

--- frontend/src/scripts/manageUsers.ts
+++ frontend/src/scripts/manageUsers.ts
@@ -1,7 +1,15 @@
+declare const Swal:any;
 const session = JSON.parse(localStorage.getItem("session") || "null");
 if (!session || session.role !== "admin") {
-    alert("Access denied");
-    window.location.href = "/frontend/index.html";
+
+    Swal.fire({
+        icon: "error",
+        title: "Access Denied",
+        text: "Admin login required"
+    }).then(() => {
+        window.location.href = "/frontend/index.html";
+    });
+
 }
 
 const table = document.getElementById("user-table");
@@ -17,11 +25,15 @@ function renderUsers(list: any[] = users) {
         const row = document.createElement("tr");
         row.className = "border-b hover:bg-gray-50";
         row.innerHTML = `
-<td class="p-3 text-center">${user.name}</td><td class="p-3 text-center">${user.email}</td><td class="p-3 text-center"><button
+<td class="p-3 text-center">${user.name}</td>
+<td class="p-3 text-center">${user.email}</td>
+<td class="p-3 text-center">
+<button
 class="delete-btn bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
 data-id="${user.id}">
 Delete
-</button></td>`;
+</button>
+</td>`;
         table.appendChild(row);
     });
     if (userCount) {
@@ -34,12 +46,23 @@ document.addEventListener("click", (e) => {
     const target = e.target as HTMLElement;
     if (target.classList.contains("delete-btn")) {
         const id = Number(target.getAttribute("data-id"));
-        const confirmDelete = confirm("Are you sure you want to delete this user?");
-        if (!confirmDelete) return;
+        let user = users.find((u:any)=> u.id === id);
+        Swal.fire({
+            title: "Delete User?",
+            text: "This action cannot be undone",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            confirmButtonText: "Delete"
+        }).then((result: any) => {
+        if (!result.isConfirmed) return;
         users = users.filter((u: any) => u.id !== id);
         localStorage.setItem("users", JSON.stringify(users));
+        let bookings= JSON.parse(localStorage.getItem("bookings")|| "[]");
+        bookings= bookings.filter((b:any)=> b.userEmail!== user.email);
+        localStorage.setItem("bookings",JSON.stringify(bookings));
         renderUsers();
-    }
+    })}
 });
 
 /* SEARCH USER */
